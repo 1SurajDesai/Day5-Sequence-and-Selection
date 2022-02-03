091c95b35d65ca8fa0281047e650a0c294b69636
1SurajDesai <surajdesai219@gmail.com>
2022-02-03 08:51:40 +0530

    Adding a file

---
+++ No_In_Word_Using_Case.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+x=$((RANDOM%10))
+
+case $x in
+	1 )
+		echo "one"
+		;;
+	2 )
+		echo "two"
+		;;
+	3 )
+		echo "three"
+		;;
+	4 )
+		echo "four"
+		;;
+	5 )
+		echo "five"
+		;;
+	6 )
+		echo "six"
+		;;
+	7 )
+		echo "seven"
+		;;
+	8 )
+		echo "eight"
+		;;
+	9 )
+		echo "nine"
+		;;
+	0 )
+		echo "zero"
+		;;
+	*)
+		echo "Enter correct number"
+esac
